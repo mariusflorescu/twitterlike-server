cd31d3d1d4e21642aaf8751d1f09267b9ec855c6
Marius Florescu <mariusflorescu@Mariuss-MacBook-Pro.local>
2020-11-15 20:58:19 +0200

    Server done!

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://mariusflorescu:2oxumClPAJZf4WuU@tcluster0.rd7mo.mongodb.net/twitterlike?retryWrites=true&w=majority',
+  SECRET_KEY: 'SECRETMDBXD'
+}
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,23 @@
+const postsResolvers = require('./posts');
+const userResolvers = require('./users');
+const commentsResolvers = require('./comments');
+const posts = require('./posts');
+
+module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length
+  },
+  Query: {
+    ...postsResolvers.Query,
+    ...userResolvers.Query
+  },
+  Mutation:{
+    ...userResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
+  },
+  Subscription:{
+    ...postsResolvers.Subscription
+  }
+}
